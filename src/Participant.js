ae75fffe457e9f553f3c43a90b681b8735b4c788
Payman Kossari <Paymankossari@gmail.com>
2019-02-05 13:31:48 -0500

    session participant list

--- src/Participant.js
+++ src/Participant.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-export default function Participant(props){
-    return (
-        
-    );
+export default function Participant(props) {
+  let statusHTML = props.inSession
+    ? props.onStage
+      ? 'on stage'
+      : 'in session'
+    : 'left session';
+  return (
+    <li className="session-participant">
+      <img src={props.avatar} alt={`participant of {altText}`} />
+      <div className="participant-info">
+        <h6>{props.name}</h6>
+        <div className="participant-status">{statusHTML};</div>
+      </div>
+    </li>
+  );
 }
--- src/ParticipantList.js
+++ src/ParticipantList.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import Participant from './Participant'
+import Participant from './Participant';
 
+export default function ParticipantList(props) {
+  const participants = props.participants;
+  let participantsHTML = [];
 
-export default function ParticipantList(props){
-    const participants = props.participants;
-    let participantsHTML = '';
-
-    participants.filter(element => element.inSession).forEach(element => {
-        participantsHTML += <Participant key={element.id} name={element.name} avatar={element.avatar} inSession={true}/>
-    })
-    participants.filter(element => !element.inSession).forEach(element => {
-        participantsHTML += <Participant key={element.id} name={element.name} avatar={element.avatar} inSession={false}/>
-    })
-    return(
-        <ul>
-            {participantsHTML}
-        </ul>
-    );
+  participants
+    .filter(element => element.inSession)
+    .forEach(element => {
+      debugger;
+      participantsHTML.push(
+        <Participant
+          key={element.id}
+          name={element.name}
+          avatar={element.avatar}
+          inSession={true}
+        />
+      );
+    });
+  participants
+    .filter(element => !element.inSession)
+    .forEach(element => {
+      participantsHTML.push(
+        <Participant
+          key={element.id}
+          name={element.name}
+          avatar={element.avatar}
+          inSession={false}
+        />
+      );
+    });
+  return <ul>{participantsHTML}</ul>;
 }
-
