58f36e180fdf9060b7dec1c3060e58317aff32b9
Andrew Bituin <fandrewbituin@gmail.com>
2019-02-05 09:19:59 -0800

    working on Participant List

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import ParticipantList from './ParticipantList';
+import store from './store';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="sidebar">
+        <ParticipantList participants={store.participants}/>
+      </div>
+    ); 
+  }
+}
+
+export default App;
---
+++ src/ParticipantList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Participant from './Participant'
+
+
+export default function ParticipantList(props){
+    const participants = props.participants;
+    let participantsHTML = '';
+
+    participants.filter(element => element.inSession).forEach(element => {
+        participantsHTML += <Participant key={element.id} name={element.name} avatar={element.avatar} inSession={true}/>
+    })
+    participants.filter(element => !element.inSession).forEach(element => {
+        participantsHTML += <Participant key={element.id} name={element.name} avatar={element.avatar} inSession={false}/>
+    })
+    return(
+        <ul>
+            {participantsHTML}
+        </ul>
+    );
+}
+
